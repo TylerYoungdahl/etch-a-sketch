6a0f625b7933b591f206f74b489500f1ba9533df
tyler <tylerjyoungdahl@gmail.com>
2024-02-27 18:29:39 -0800

    made basic etch a sketch

--- scripts.js
+++ scripts.js
@@ -1,17 +1,36 @@
 const container = document.querySelector("#container");
+const newGridBtn = document.querySelector("#newGridBtn");
 
-// let gridNum = 16;
+let gridSize = 16;
 
 function makeGrid(gridNum) {
+  container.innerHTML = "";
+  let squareSize = 960 / gridNum;
+
   for (let i = 0; i < gridNum * gridNum; i++) {
     const square = document.createElement("div");
-
     square.classList.add("square");
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "black";
+    });
 
     container.appendChild(square);
   }
 }
 
-makeGrid(16);
+newGridBtn.addEventListener("click", () => {
+  let newGridSize = prompt("Enter new grid size: (1-100)");
+  if (newGridSize !== "") {
+    newGridSize = parseInt(newGridSize);
+    if (newGridSize > 0 && newGridSize <= 100) {
+      gridSize = newGridSize;
+      makeGrid(gridSize);
+    } else {
+      alert("invalid number");
+    }
+  }
+});
 
-container.addEventListener();
+makeGrid(gridSize);
